223bd85888a559b2f629ed938c1879768c28a0de
Anh Phat <anhphat@Anphacats-MacBook-Pro.local>
2021-05-26 13:29:43 +0700

    update

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+// COMPONENTS
+import AlertMsg from "../components/AlertMsg";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+// ROUTES
+import AdminLayout from "./AdminLayout";
+import PublicLayout from "./PublicLayout";
+import ProtectedRoute from "./ProtectedRoute";
+
 const Routes = () => {
   return <div>something</div>;
 };
